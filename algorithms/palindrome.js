892a97fcf0c67322efbff349e257811db09467da
Matthew <matthewhcbates@gmail.com>
2022-07-21 13:18:10 -0600

    added isPalindrome algorithm

---
+++ algorithms/palindrome.js
@@ -0,0 +1,14 @@
+// write a functio nthat will receive one argument, a string
+// function should return true if the string is a palindrom (reads the same forwards/backwards)
+// return false otherwise
+
+function isPalindrom(str) {
+  if (str === "" || str.length === 0) return false;
+
+  const regex = /[\W_]/g;
+  const lowerCaseRegex = str.toLowerCase().replace(regex, "");
+  const reverseLowerCase = lowerCaseRegex.split("").reverse().join("");
+  return lowerCaseRegex === reverseLowerCase;
+}
+
+console.log(isPalindrom("racecar"));
