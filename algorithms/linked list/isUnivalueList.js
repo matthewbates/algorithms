7dfe66785e94fe59e58054607a2ef94de0282951
Matthew <matthewhcbates@gmail.com>
2022-08-02 15:00:55 -0600

    added interview question/isUnivalueList

---
+++ algorithms/linked list/isUnivalueList.js
@@ -0,0 +1,30 @@
+// write a function that takes in the head of a linked list as an argument
+// the function should return a boolean indicating whether or not the linked
+// list contains exactly one unique value
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const u = new Node(2);
+const v = new Node(2);
+const w = new Node(3);
+const x = new Node(3);
+const y = new Node(2);
+
+// *iterative
+// function isUnivalueList(head) {
+//   let current = head;
+
+//   while (current !== null) {
+//     if (current.val !== head.val) return false;
+//     current = current.next;
+//   }
+
+//   return true;
+// }
+
+console.log(isUnivalueList(u));
